87e3cefeb109a36372841a544e51eebcd9828fcf
hemanth-sunkireddy <hemanth-sunkireddy@users.noreply.github.com>
2025-10-05 10:03:31 +0000

    deploy: ed83330755263c4911e366fdd2ee5f4836d87f34

--- storage_manager/raw_page/sidebar-items.js
+++ storage_manager/raw_page/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["ITEM_ID_SIZE","PAGE_SIZE"],"fn":["page_add_data","page_count","page_free_space"],"struct":["Page"]};
+window.SIDEBAR_ITEMS = {"constant":["ITEM_ID_SIZE","PAGE_HEADER_SIZE","PAGE_SIZE"],"fn":["init_page","page_add_data","page_count","page_free_space"],"struct":["Page"]};
--- trait.impl/core/marker/trait.Freeze.js
+++ trait.impl/core/marker/trait.Freeze.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["storage_manager",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.89.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"storage_manager/raw_page/struct.Page.html\" title=\"struct storage_manager::raw_page::Page\">Page</a>",1,["storage_manager::raw_page::Page"]]]]]);
+    var implementors = Object.fromEntries([["storage_manager",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.90.0/core/marker/trait.Freeze.html\" title=\"trait core::marker::Freeze\">Freeze</a> for <a class=\"struct\" href=\"storage_manager/raw_page/struct.Page.html\" title=\"struct storage_manager::raw_page::Page\">Page</a>",1,["storage_manager::raw_page::Page"]]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
